145f25bbd38a363c35f9e8ec77aa73fe4dab4168
cheddars <nezahrish@gmail.com>
2021-11-17 00:20:22 +0900

    initial table

---
+++ vue-bootstrap/src/components/InfiniteTable.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="infini-table">
+    <table v-scroll="handleScroll">
+      <thead>
+      <tr>
+        <th v-for="(field, idx) in fields" v-bind:key="idx">{{ field }}</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item, idx) in items" v-bind:key="idx">
+        <td v-for="(field, f_idx) in fields" v-bind:key="f_idx">{{item[field]}}</td>
+      </tr>
+      </tbody>
+      <tfoot>
+      <tr v-if="isLoading">
+        <td :colspan="fields.length">
+          <b-icon icon="arrow-clockwise" animation="spin" font-scale="2"></b-icon>
+        </td>
+      </tr>
+      </tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import debounce from 'lodash/debounce'
+
+export default {
+    name: "InfiniteTable",
+    data: () => {
+        return {
+                  isLoading : true,
+      fields : ["test", "abc"],
+      items: [
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+        {"test" : 1, "abc" : 10},
+      ]
+    }
+  },
+  methods: {
+    setIsLoading(loading){
+      this.isLoading = loading;
+    },
+    appendItems(){
+      this.items.push(
+        [{"test" : 2, "abc" : 100}]
+      )
+    },
+    handleScroll(event) {
+      console.log(event);
+      this.isUserScrolling = (window.scrollY > 0);
+      console.log('calling handleScroll');
+      debounce()
+    }
+  }
+}
+</script>
+
+<style>
+.infini-table {
+  width: 90%;
+  height: 600px;
+}
+
+table {
+  display: block;
+  width: 100%;
+  height: 300px;
+  overflow: auto;
+}
+th { position: sticky; top: 0; z-index: 1; }
+tfoot td { position: sticky; bottom:0; z-index: 1;}
+
+/* Just common table stuff. Really. */
+table  { border-collapse: collapse; width: 100%; }
+th, td { padding: 8px 16px; }
+th     { background:#eee; }
+
+</style>
--- vue-bootstrap/src/main.js
+++ vue-bootstrap/src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.directive('scroll', {
+  inserted: function (el, binding) {
+    let f = function (evt) {
+      if (binding.value(evt, el)) {
+        window.removeEventListener('scroll', f)
+      }
+    }
+    window.addEventListener('scroll', f)
+  }
+})
 
 new Vue({
   render: h => h(App),
