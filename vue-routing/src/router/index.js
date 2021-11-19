60d9cb04ad6bc65fa2063261fc7b4e4c0239f95c
cheddarz <nezahrish@gmail.com>
2021-11-19 20:44:56 +0900

    routing closed

--- vue-routing/src/router/index.js
+++ vue-routing/src/router/index.js
@@ -8,7 +8,7 @@ import UserAComp from '@/components/UserAComp'
 import UserBComp from '@/components/UserBComp'
 import UserInfo from '@/components/UserInfo'
 
-// import { getUser, isAuthenticated } from '../service/auth/AuthService'
+import { isAuthenticated } from '../service/auth/AuthService'
 
 Vue.use(Router)
 
@@ -16,19 +16,37 @@ let router = new Router({
   mode: 'history',
   routes: [
     { path: '/login', name: 'login', component: Login },
-    { path: '/', name: 'root', component: Root },
-    { path: '/userinfo', name: 'userinfo', component: UserInfo },
-    { path: '/usera', name: 'usera', component: UserAComp },
-    { path: '/userb', name: 'userb', component: UserBComp }
+    { path: '/', name: 'root', component: Root, meta: { reqAuth : true }},
+    { path: '/userinfo', name: 'userinfo', component: UserInfo, meta: { reqAuth: true } },
+    { path: '/usera', name: 'usera', component: UserAComp, meta: { reqAuth: true, type : 'typeA' } },
+    { path: '/userb', name: 'userb', component: UserBComp, meta: { reqAuth: true, type : 'typeB' } }
   ]
 })
 
-// router.beforeEach((to, from, next) => {
-//   if (isAuthenticated()) {
-//     to.params.user = getUser()
-//     console.log('is_auth', to.params.user.username)
-//   }
-//   next()
-// })
+router.beforeEach((to, from, next) => {
+  console.log("check route", to)
+  console.log("is authenticated() :", isAuthenticated() === true)
+  const role = 'typeA'
+
+  if (to.matched.some(record => record.meta.reqAuth)) {
+    console.log("require auth")
+    if (!isAuthenticated()) {
+      console.log("not logined")
+      next({name : 'login'})
+    } else {
+      console.log("logined")
+
+      if(to.matched.some(record => { return (!record.meta.type || record.meta.type == role) })){
+        console.log("allowed", role)
+        next()        
+      }else{
+        console.log("not allowed", role)
+        next({name : 'root' })
+      }
+    }
+  } else {
+    next() // 반드시 next()를 호출하십시오!
+  }
+})
 
 export default router
--- vue-routing/src/service/auth/AuthService.js
+++ vue-routing/src/service/auth/AuthService.js
@@ -1,10 +1,11 @@
 export const isAuthenticated = () => {
-    // const token = localStorage.getItem("access_token");
-    return true;
-  }
-
+    const isLogin = localStorage.getItem("is_login")
+    console.log(isLogin)
+    return isLogin === 'true'
+}
+ 
 export const getUser = () => {
-    var token = localStorage.getItem("access_token");
+    var token = localStorage.getItem("user_info");
 
     return JSON.parse(token);
 }
